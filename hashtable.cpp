364eab92bec756cbdad6f61e09ccabf57f44447e
alenissmart <alen112.scientist@gmail.com>
2025-11-02 22:03:49 -0500

    completed hashtable.cpp

--- hashtable.cpp
+++ hashtable.cpp
@@ -19,3 +19,52 @@ size_t HashTable::indexFor(const string& key) const {
     size_t index = hashValue % buckets_.size();
     return index;
 }
+
+bool HashTable::insert(const string& key) {
+    size_t index = indexFor(key);
+    vector<string>& chain = buckets_[index];
+
+    for (size_t i = 0; i < chain.size(); i++) {
+        if (chain[i] == key) {
+            return false;
+        } 
+    }
+
+    chain.push_back(key);
+    size_ += 1;
+    return true;
+}
+
+bool HashTable::contains(const string& key) const {
+    size_t index = indexFor(key);
+    const vector<string>& chain = buckets_[index];
+
+    for (size_t i = 0; i < chain.size(); i++) {
+        if (chain[i] == key) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool HashTable::erase(const string& key) {
+    size_t index = indexFor(key);
+    vector<string>& chain = buckets_[index];
+
+    for (size_t i = 0; i < chain.size(); i++) {
+        if (chain[i] == key) {
+            if (i+1 != chain.size()) {
+                string temp = chain.back();
+                chain[i] = temp;
+            }
+
+            chain.pop_back();
+
+            size_ -= 1;
+
+            return true;
+        }
+    }
+    return false;
+}
