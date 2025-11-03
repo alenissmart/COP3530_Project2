c82bece129890d6b505e56735f4dc8b356b99368
alenissmart <alen112.scientist@gmail.com>
2025-11-02 20:33:26 -0500

    started on implementing the hash table

--- hashtable.cpp
+++ hashtable.cpp
@@ -1,1 +1,21 @@
 #include "hashtable.h"
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+HashTable::HashTable(size_t bucketCount) {
+    if (bucketCount == 0) {
+        bucketCount = 8;
+    }
+
+    buckets_.resize(bucketCount);
+    size_ = 0;
+}
+
+size_t HashTable::indexFor(const string& key) const {
+    hash<string> hasher;
+    size_t hashValue = hasher(key);
+    size_t index = hashValue % buckets_.size();
+    return index;
+}
